0c8bc32855720349b43a19ffab647ac7d3c14a93
ZuriPL <michal.bak222@gmail.com>
2022-08-24 15:40:11 +0200

    microtica

--- config/database.js
+++ config/database.js
@@ -1,11 +1,53 @@
-const path = require('path');
+// const path = require('path');
 
-module.exports = ({ env }) => ({
-  connection: {
-    client: 'sqlite',
-    connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+// module.exports = ({ env }) => ({
+//   connection: {
+//     client: 'sqlite',
+//     connection: {
+//       filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+//     },
+//     useNullAsDefault: true,
+//   },
+// });
+
+module.exports = ({ env }) => {
+  const connections = {
+    sqlite: {
+      connection: {
+        client: "sqlite",
+        connection: {
+          filename: env("DATABASE_FILENAME", ".tmp/data.db"),
+        },
+        useNullAsDefault: true,
+      },
     },
-    useNullAsDefault: true,
-  },
-});
+    mysql: {
+      connection: {
+        client: "mysql",
+        connection: {
+          host: env("DATABASE_HOST", "localhost"),
+          port: env.int("DATABASE_PORT", 3306),
+          database: env("DATABASE_NAME", "strapi"),
+          user: env("DATABASE_USERNAME", "strapi"),
+          password: env("DATABASE_PASSWORD", "strapi"),
+        },
+        debug: false,
+      },
+    },
+    postgres: {
+      connection: {
+        client: "postgres",
+        connection: {
+          host: env("DATABASE_HOST", "localhost"),
+          port: env.int("DATABASE_PORT", 3306),
+          database: env("DATABASE_NAME", "strapi"),
+          user: env("DATABASE_USERNAME", "strapi"),
+          password: env("DATABASE_PASSWORD", "strapi"),
+        },
+        debug: false,
+      },
+    },
+  };
+
+  return connections[env("DATABASE_CLIENT")];
+};
